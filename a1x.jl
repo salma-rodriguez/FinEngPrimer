61ce4cebf7113fc3b38a9cc70c228d49ecfddba5
salma-rodriguez <salmayrodriguez@gmail.com>
2022-01-06 15:20:41 -0500

    some deletes

--- a1x.jl
+++
@@ -1,7 +0,0 @@
-function d1(t, S, K, T, σ, r, q)
-    return ((log(S / K) + (r - q + σ^2 / 2)) * (T - t)) / (σ * sqrt(T - t))
-end
-
-function d2(t, S, K, T, σ, r, q)
-    return d1 - σ * sqrt(T - t)
-end
